820f90b451f0be2d8c8ba0ccb9cabe89ecedb88b
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-02-08 22:58:59 +0200

    Add post page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def showblog(request):
+    posts = Post.objects
+    return render(request, 'blog/blog.html', {'posts': posts})
